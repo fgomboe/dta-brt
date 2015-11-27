f5f4865012a94828d0ae9ecf1f4a17551422192a
Andres Gomez <fgomboe@gmail.com>
2015-11-27 11:25:02 -0500

    Add new package and new output directory
    Add package 'statistics' to the project in Eclipse, along with a new
    source file to make a table containing all jorneys and their travel and
    waiting times.
    
    Add output direcotry 'output20_BestScore70_Reroute30, taken from previous
    working.

---
+++ src/main/java/edu/univalle/statistics/JourneyTimes.java
@@ -0,0 +1,12 @@
+package edu.univalle.statistics;
+
+public class JourneyTimes {
+	
+	private String eventsFiles = ""
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
