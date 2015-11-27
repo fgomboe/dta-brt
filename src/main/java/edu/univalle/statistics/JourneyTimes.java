d03165f345148f4aa9562fc715d5fb0aa3a8b84f
Andres Gomez <fgomboe@gmail.com>
2015-11-27 15:31:00 -0500

    Modify file 'JourneyTimes.java'
    File in construction.

--- src/main/java/edu/univalle/statistics/JourneyTimes.java
+++ src/main/java/edu/univalle/statistics/JourneyTimes.java
@@ -1,12 +1,34 @@
 package edu.univalle.statistics;
 
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.events.EventsReaderXMLv1;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.scoring.EventsToLegs;
+
 public class JourneyTimes {
 	
-	private String eventsFiles = ""
+	private String eventsFiles = "output20_BestScore70_Reroute30/ITERS/it.20";
+	private Config config;
+	private Scenario scenario;
+	private EventsReaderXMLv1 eventsReader;
+	private EventsToLegs manager;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	}
+	
+	public void init() {
+        this.config = ConfigUtils.loadConfig("input/config.xml");
+        this.scenario = ScenarioUtils.createScenario(config);
+        this.manager = new EventsToLegs(scenario);
+        this.eventsReader = new EventsReaderXMLv1();
+        
+	}
+	
+	public void calculate() {
+	    //TODO use manager object to handle events file and compute statistics on legs
 	}
 
 }
