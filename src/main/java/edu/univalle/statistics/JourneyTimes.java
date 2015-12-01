9a56e1a6e26947ca38b42f85067b25572bcee148
Andres Gomez <fgomboe@gmail.com>
2015-12-01 15:24:15 -0500

    Change file 'JourneyTimes.java'
    Change file 'JourneyTimes.java' in charge of computing statistical
    information on the journey times of the system.
    
    The change adds an EventsManagar to the EventsReaderXMLv1 object.

--- src/main/java/edu/univalle/statistics/JourneyTimes.java
+++ src/main/java/edu/univalle/statistics/JourneyTimes.java
@@ -1,19 +1,22 @@
 package edu.univalle.statistics;
 
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.api.experimental.events.EventsManager;
 import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.events.EventsReaderXMLv1;
+import org.matsim.core.events.EventsUtils;
+import org.matsim.core.events.handler.BasicEventHandler;
 import org.matsim.core.scenario.ScenarioUtils;
 import org.matsim.core.scoring.EventsToLegs;
 
-public class JourneyTimes {
+public class JourneyTimes implements BasicEventHandler{
 	
 	private String eventsFiles = "output20_BestScore70_Reroute30/ITERS/it.20";
 	private Config config;
 	private Scenario scenario;
 	private EventsReaderXMLv1 eventsReader;
-	private EventsToLegs manager;
+    private final EventsManager e = EventsUtils.createEventsManager();
 
 	public static void main(String[] args) {
 
@@ -23,7 +26,11 @@ public class JourneyTimes {
         this.config = ConfigUtils.loadConfig("input/config.xml");
         this.scenario = ScenarioUtils.createScenario(config);
         this.manager = new EventsToLegs(scenario);
-        this.eventsReader = new EventsReaderXMLv1();
+        //TODO manage how to work with the event handler "e" 
+        //hint: see file "EventsFileComparator.java" in package "org.matsim.utils.eventsfilecomparison"
+        //      and file "Worker.java" in package "org.matsim.utils.eventsfilecomparison" 
+        e.addHandler(this);
+        this.eventsReader = new EventsReaderXMLv1(e);
         
 	}
 	
