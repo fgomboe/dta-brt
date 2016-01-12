38d6aa2b008e3454ed098da8278a4c39cd75c849
Andres Gomez <fgomboe@gmail.com>
2016-01-12 17:38:19 -0500

    Change file 'CreateNetworkSHP.java' to better suit requirements
    - Change file 'CreateNetworkSHP.java' to delete redundant id created
    for visum and include only the required stations to visum, not the
    different bays on each station.
    - Add file 'std_code.csv', which contains the codification for stations.
    - Create new shapefiles according to the new rules (previous files were
    preserved in folder 'Cali_special_generated_trunk_network_BAD_IDs').

--- src/main/java/edu/univalle/network/CreateNetworkSHP.java
+++ src/main/java/edu/univalle/network/CreateNetworkSHP.java
@@ -3,7 +3,6 @@ package edu.univalle.network;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.TreeMap;
 
 import org.matsim.api.core.v01.Scenario;
 import org.matsim.api.core.v01.network.Link;
@@ -23,103 +22,102 @@ import org.opengis.referencing.crs.CoordinateReferenceSystem;
 
 import com.vividsolutions.jts.geom.Coordinate;
 
+import edu.univalle.utils.CsvReader;
 import edu.univalle.utils.Utils;
 
-
-public class CreateNetworkSHP {
+public class CreateNetworkSHP
+{
 
     public static void main(String[] args) throws Exception {
-   
-        Config config = ConfigUtils.createConfig();
-        config.network().setInputFile("output/specialNetwork.xml");
+
+        String configFile = "input/config.xml";
+        Config config = ConfigUtils.loadConfig(configFile);
         Scenario scenario = ScenarioUtils.loadScenario(config);
         Network network = scenario.getNetwork();
-        
+
         ObjectAttributes linkAttributes = new ObjectAttributes();
         ObjectAttributes nodeAttributes = new ObjectAttributes();
-        new ObjectAttributesXmlReader(linkAttributes).parse("./output/specialLinkAttributes.xml");
-        new ObjectAttributesXmlReader(nodeAttributes).parse("./output/specialNodeAttributes.xml");
-             
+        new ObjectAttributesXmlReader(linkAttributes).parse("./input/specialLinkAttributes.xml");
+        new ObjectAttributesXmlReader(nodeAttributes).parse("./input/specialNodeAttributes.xml");
+        String std_codeFile = "./input/networkMIO/std_code.csv";    // File with coding for stations, according to detailed card validations of Metrocali
+
         CoordinateReferenceSystem crs = MGC.getCRS("EPSG:3115");		// EPSG Code for MAGNA-SIRGAS / Colombia West Zone coordinate system
 
         Collection<SimpleFeature> features = new ArrayList<SimpleFeature>();
-        PointFeatureFactory nodeFactory = new PointFeatureFactory.Builder().
-                setCrs(crs).
-                setName("nodes").
-                addAttribute("id", Integer.class).
-                addAttribute("shortName", String.class).
-                addAttribute("longName", String.class).
-                addAttribute("gpsX", Double.class).
-                addAttribute("gpsY", Double.class).
-                addAttribute("lines", String.class).
-                addAttribute("inLines", String.class).
-                addAttribute("outLines", String.class).
-                create();
+        PointFeatureFactory nodeFactory = new PointFeatureFactory.Builder().setCrs(crs).setName("nodes")
+                .addAttribute("id", Integer.class).addAttribute("longName", String.class)
+                .addAttribute("gpsX", Double.class).addAttribute("gpsY", Double.class)
+                .addAttribute("lines", String.class).addAttribute("inLines", String.class)
+                .addAttribute("outLines", String.class).create();
 
-        TreeMap<String, Integer> nodeNames = new TreeMap<String, Integer>();
-        int id = 1;
+        CsvReader nodes = new CsvReader(std_codeFile);
+        nodes.readHeaders();
+
         for (Node node : network.getNodes().values()) {
-        	nodeNames.put(node.getId().toString(), id);
-        	String longName = node.getId().toString().length() <= 2 ? 
-        						(String) nodeAttributes.getAttribute(node.getId().toString(), "ESTACION") : 
-        						(String) nodeAttributes.getAttribute(node.getId().toString(), "LONGNAME");
-        	Double gpsX = node.getId().toString().length() <= 2 ? 
-	        				(Double) nodeAttributes.getAttribute(node.getId().toString(), "GPS_X") : 
-	        				(Double) nodeAttributes.getAttribute(node.getId().toString(), "LONGITUDE");
-        	Double gpsY = node.getId().toString().length() <= 2 ? 
-	        				(Double) nodeAttributes.getAttribute(node.getId().toString(), "GPS_Y") : 
-	        				(Double) nodeAttributes.getAttribute(node.getId().toString(), "LATITUDE");
-        	String lines = (String) nodeAttributes.getAttribute(node.getId().toString(), "LINES");
-        	String inLines = (String) nodeAttributes.getAttribute(node.getId().toString(), "IN_LINES");
-        	String outLines = (String) nodeAttributes.getAttribute(node.getId().toString(), "OUT_LINES");
-            SimpleFeature ft = nodeFactory.createPoint(node.getCoord(), new Object[] {id, node.getId().toString(), longName, gpsX, gpsY, lines != null ? lines.substring(1, lines.length() - 1) : lines, inLines != null ? inLines.substring(1, inLines.length() - 1) : inLines, outLines != null ? outLines.substring(1, outLines.length() - 1) : outLines}, null);
-            // Comment the following 3 lines to export entire network
-            List<String> linesArray = new ArrayList<String>();
-            linesArray = Utils.parseToList(lines);
-            if (linesArray.contains("E21") || linesArray.contains("E31") || linesArray.contains("T31"))
-            	features.add(ft);
-            id++;
+            if (node.getId().toString().length() <= 2) {
+                int id = Integer.parseInt(node.getId().toString());
+                String longName = (String) nodeAttributes.getAttribute(node.getId().toString(), "ESTACION");
+                Double gpsX = (Double) nodeAttributes.getAttribute(node.getId().toString(), "GPS_X");
+                Double gpsY = (Double) nodeAttributes.getAttribute(node.getId().toString(), "GPS_Y");
+                String lines = (String) nodeAttributes.getAttribute(node.getId().toString(), "LINES");
+                String inLines = (String) nodeAttributes.getAttribute(node.getId().toString(), "IN_LINES");
+                String outLines = (String) nodeAttributes.getAttribute(node.getId().toString(), "OUT_LINES");
+                SimpleFeature ft = nodeFactory.createPoint(node.getCoord(),
+                        new Object[] { id, longName, gpsX, gpsY,
+                                lines != null ? lines.substring(1, lines.length() - 1) : lines,
+                                inLines != null ? inLines.substring(1, inLines.length() - 1) : inLines,
+                                outLines != null ? outLines.substring(1, outLines.length() - 1) : outLines },
+                        null);
+                // Comment the following 3 lines to export entire network
+                List<String> linesArray = new ArrayList<String>();
+                linesArray = Utils.parseToList(lines);
+                if (linesArray.contains("E21") || linesArray.contains("E31") || linesArray.contains("T31"))
+                    features.add(ft);
+            }
         }
-        ShapeFileWriter.writeGeometries(features, "input/shapes/Cali_special_generated_trunk_network/network_nodes.shp");
-        
-        
-        features = new ArrayList<SimpleFeature>();
-        PolylineFeatureFactory linkFactory = new PolylineFeatureFactory.Builder().
-                setCrs(crs).
-                setName("link").
-                addAttribute("id", Integer.class).
-                addAttribute("name", String.class).
-//                addAttribute("fromNode", String.class).
-                addAttribute("fromNode", Integer.class).
-//                addAttribute("toNode", String.class).
-                addAttribute("toNode", Integer.class).
-                addAttribute("length", Double.class).
-                addAttribute("freespeed", Double.class).
-                addAttribute("capacity", Double.class).
-                addAttribute("noOfLanes", Integer.class).
-                addAttribute("lines", String.class).
-                addAttribute("line_route", String.class).
-                create();
+        ShapeFileWriter.writeGeometries(features,
+                "input/shapes/Cali_special_generated_trunk_network/network_nodes.shp");
 
-        id = 1;
+        features = new ArrayList<SimpleFeature>();
+        PolylineFeatureFactory linkFactory = new PolylineFeatureFactory.Builder().setCrs(crs).setName("link")
+                .addAttribute("id", Integer.class).addAttribute("name", String.class).
+                // addAttribute("fromNode", String.class).
+                addAttribute("fromNode", Integer.class).
+                // addAttribute("toNode", String.class).
+                addAttribute("toNode", Integer.class).addAttribute("length", Double.class)
+                .addAttribute("freespeed", Double.class).addAttribute("capacity", Double.class)
+                .addAttribute("noOfLanes", Integer.class).addAttribute("lines", String.class)
+                .addAttribute("line_route", String.class).create();
+
+        int id = 1;
         for (Link link : network.getLinks().values()) {
-        	String fromNode = link.getFromNode().getId().toString();
-        	String toNode = link.getToNode().getId().toString();
-            Coordinate fromNodeCoordinate = new Coordinate(link.getFromNode().getCoord().getX(), link.getFromNode().getCoord().getY());
-            Coordinate toNodeCoordinate = new Coordinate(link.getToNode().getCoord().getX(), link.getToNode().getCoord().getY());
-            Coordinate linkCoordinate = new Coordinate(link.getCoord().getX(), link.getCoord().getY());
-            String linesAtt = (String) linkAttributes.getAttribute(link.getId().toString(), "LINES");
-            String linesRoutesAtt = (String) linkAttributes.getAttribute(link.getId().toString(), "LINE_ROUTE");
-            SimpleFeature ft = linkFactory.createPolyline(new Coordinate [] {fromNodeCoordinate, linkCoordinate, toNodeCoordinate},
-//                    new Object [] {id, link.getId().toString(), link.getFromNode().getId().toString(), link.getToNode().getId().toString(), link.getLength(), link.getFreespeed(), link.getCapacity(), link.getNumberOfLanes(), linesAtt.substring(1, linesAtt.length() - 1), linesRoutesAtt.substring(1, linesRoutesAtt.length() - 1)}, null);
-            		new Object [] {id, link.getId().toString(), nodeNames.get(fromNode), nodeNames.get(toNode), link.getLength(), link.getFreespeed(), link.getCapacity(), link.getNumberOfLanes(), linesAtt.substring(1, linesAtt.length() - 1), linesRoutesAtt.substring(1, linesRoutesAtt.length() - 1)}, null);
-         // Comment the following 3 lines to export entire network
-            List<String> linesArray = new ArrayList<String>();
-            linesArray = Utils.parseToList(linesAtt);
-            if (linesArray.contains("E21") || linesArray.contains("E31") || linesArray.contains("T31"))
-            	features.add(ft);
-            id++;
-        }   
-        ShapeFileWriter.writeGeometries(features, "input/shapes/Cali_special_generated_trunk_network/network_links.shp");
+            String fromNode = link.getFromNode().getId().toString();
+            String toNode = link.getToNode().getId().toString();
+            if (fromNode.length() <= 2 && toNode.length() <= 2) {
+                Coordinate fromNodeCoordinate = new Coordinate(link.getFromNode().getCoord().getX(),
+                        link.getFromNode().getCoord().getY());
+                Coordinate toNodeCoordinate = new Coordinate(link.getToNode().getCoord().getX(),
+                        link.getToNode().getCoord().getY());
+                Coordinate linkCoordinate = new Coordinate(link.getCoord().getX(), link.getCoord().getY());
+                String linesAtt = (String) linkAttributes.getAttribute(link.getId().toString(), "LINES");
+                String linesRoutesAtt = (String) linkAttributes.getAttribute(link.getId().toString(), "LINE_ROUTE");
+                SimpleFeature ft = linkFactory.createPolyline(
+                        new Coordinate[] { fromNodeCoordinate, linkCoordinate, toNodeCoordinate },
+                        // new Object [] {id, link.getId().toString(), link.getFromNode().getId().toString(), link.getToNode().getId().toString(), link.getLength(), link.getFreespeed(), link.getCapacity(), link.getNumberOfLanes(), linesAtt.substring(1, linesAtt.length() - 1), linesRoutesAtt.substring(1, linesRoutesAtt.length() - 1)}, null);
+                        new Object[] { id, link.getId().toString(), fromNode, toNode, link.getLength(),
+                                link.getFreespeed(), link.getCapacity(), link.getNumberOfLanes(),
+                                linesAtt.substring(1, linesAtt.length() - 1),
+                                linesRoutesAtt.substring(1, linesRoutesAtt.length() - 1) },
+                        null);
+                // Comment the following 3 lines to export entire network
+                List<String> linesArray = new ArrayList<String>();
+                linesArray = Utils.parseToList(linesAtt);
+                if (linesArray.contains("E21") || linesArray.contains("E31") || linesArray.contains("T31"))
+                    features.add(ft);
+                id++;
+            }
+        }
+        ShapeFileWriter.writeGeometries(features,
+                "input/shapes/Cali_special_generated_trunk_network/network_links.shp");
     }
 }
