fc6d7ee4d626639eeec80f1653f1dd22f368e211
Andres Gomez <fgomboe@gmail.com>
2016-01-31 13:33:03 -0500

    Change format of class 'CreateTransitScheduleMIO.java' and transportMode to 'pt'

--- src/main/java/edu/univalle/transit/CreateTransitScheduleMIO.java
+++ src/main/java/edu/univalle/transit/CreateTransitScheduleMIO.java
@@ -30,179 +30,184 @@ import edu.univalle.utils.CsvReader;
 public class CreateTransitScheduleMIO
 {
 
-	private TransitSchedule schedule;
-	private Scenario scenario;
-	private Network network;
-	
-	private String networkFile = "./input/specialNetwork.xml";
-	private String nodeAttributesFile = "./input/specialNodeAttributes.xml";
-	
-	private String csvArcs = "./input/networkMIO/arcs.csv";
+    private TransitSchedule schedule;
+    private Scenario scenario;
+    private Network network;
 
-	private ObjectAttributes nodeAttributes = new ObjectAttributes();
+    private String networkFile = "./input/specialNetwork.xml";
+    private String nodeAttributesFile = "./input/specialNodeAttributes.xml";
 
-	public static void main(String[] args)
-	{
+    private String csvArcs = "./input/networkMIO/arcs.csv";
 
-		CreateTransitScheduleMIO createTransit = new CreateTransitScheduleMIO();
-		createTransit.init();
-		createTransit.createSchedule();
-		createTransit.write();
-	}
+    private ObjectAttributes nodeAttributes = new ObjectAttributes();
 
-	public void init()
-	{
-		this.scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
-		new NetworkReaderMatsimV1(this.scenario).parse(networkFile);
-		this.network = scenario.getNetwork();
-		//this.scenario.getConfig().scenario().setUseTransit(true);
-		//this.scenario.getConfig().scenario().setUseVehicles(true);
-		schedule = this.scenario.getTransitSchedule();
-	}
+    public static void main(String[] args) {
 
-	public void write()
-	{
-		new TransitScheduleWriterV1(schedule).write("./output/transitSchedule.xml");
-	}
+        CreateTransitScheduleMIO createTransit = new CreateTransitScheduleMIO();
+        createTransit.init();
+        createTransit.createSchedule();
+        createTransit.write();
+    }
 
-	public void createSchedule()
-	{
+    public void init() {
+        this.scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        new NetworkReaderMatsimV1(this.scenario).parse(networkFile);
+        this.network = scenario.getNetwork();
+        // this.scenario.getConfig().scenario().setUseTransit(true);
+        // this.scenario.getConfig().scenario().setUseVehicles(true);
+        schedule = this.scenario.getTransitSchedule();
+    }
 
-		new ObjectAttributesXmlReader(nodeAttributes).parse(nodeAttributesFile);
+    public void write() {
+        new TransitScheduleWriterV1(schedule).write("./output/transitSchedule.xml");
+    }
 
-		Id<Link> startLink = null;
-		Id<Link> endLink = null;
-		List<Id<Link>> links = new ArrayList<Id<Link>>();
+    public void createSchedule() {
 
-		List<TransitRouteStop> stops = new ArrayList<TransitRouteStop>();
+        new ObjectAttributesXmlReader(nodeAttributes).parse(nodeAttributesFile);
 
-		String currentLine = "", currentDirection = "";
+        Id<Link> startLink = null;
+        Id<Link> endLink = null;
+        List<Id<Link>> links = new ArrayList<Id<Link>>();
 
-		TransitStopFacility tStopFacil = null;
-		TransitRouteStop tRouteStop = null;
-		NetworkRoute nRoute = null;
-		TransitRoute tRoute = null;
-		TransitLine tLine = null;
+        List<TransitRouteStop> stops = new ArrayList<TransitRouteStop>();
 
-		try {
-			CsvReader arcos = new CsvReader(csvArcs);
-			arcos.readHeaders();
+        String currentLine = "", currentDirection = "";
 
-			String line, direction;
+        TransitStopFacility tStopFacil = null;
+        TransitRouteStop tRouteStop = null;
+        NetworkRoute nRoute = null;
+        TransitRoute tRoute = null;
+        TransitLine tLine = null;
 
-			while (arcos.readRecord()) {
-				line = arcos.get("LINEA");
-				direction = arcos.get("SENTIDO");
+        try {
+            CsvReader arcos = new CsvReader(csvArcs);
+            arcos.readHeaders();
 
-				// The last condition is an ad hoc solution for the problem of the last route for the T31
-				if (line.equals("T31") || line.equals("E31") || line.equals("E21") || currentLine.equals("T31")) {
+            String line, direction;
 
-					Node iniNode = network.getNodes().get(Id.createNodeId(arcos.get("PUNTO_INI")));
-					Node endNode = network.getNodes().get(Id.createNodeId(arcos.get("PUNTO_FIN")));
+            while (arcos.readRecord()) {
+                line = arcos.get("LINEA");
+                direction = arcos.get("SENTIDO");
 
-					String iniStation = nodeAttributes.getAttribute(iniNode.getId().toString(), "ASSOCIATE_ID").toString();
-					String endStation = nodeAttributes.getAttribute(endNode.getId().toString(), "ASSOCIATE_ID").toString();
+                // The last condition is an ad hoc solution for the problem of the last route for the T31
+                if (line.equals("T31") || line.equals("E31") || line.equals("E21") || currentLine.equals("T31")) {
 
-					if (!direction.equals(currentDirection)) {
-						if (currentDirection != "") {
-							// because links contains also the endLink, otherwise it would be repeated
-							links.remove(links.size() - 1);
-							nRoute = new LinkNetworkRouteImpl(startLink, links, endLink);
-							tRoute = schedule.getFactory().createTransitRoute(Id.create(currentDirection, TransitRoute.class), nRoute, stops, "bus");
-							tLine.addRoute(tRoute);
-							startLink = null;
-							links.clear();
-							endLink = null;
-							stops.clear();
-						}
+                    Node iniNode = network.getNodes().get(Id.createNodeId(arcos.get("PUNTO_INI")));
+                    Node endNode = network.getNodes().get(Id.createNodeId(arcos.get("PUNTO_FIN")));
 
-						currentDirection = direction;
+                    String iniStation = nodeAttributes.getAttribute(iniNode.getId().toString(), "ASSOCIATE_ID")
+                            .toString();
+                    String endStation = nodeAttributes.getAttribute(endNode.getId().toString(), "ASSOCIATE_ID")
+                            .toString();
 
-						startLink = Id.createLinkId(iniStation + "-" + iniNode.getId().toString());
+                    if (!direction.equals(currentDirection)) {
+                        if (currentDirection != "") {
+                            // because links contains also the endLink, otherwise it would be repeated
+                            links.remove(links.size() - 1);
+                            nRoute = new LinkNetworkRouteImpl(startLink, links, endLink);
+                            tRoute = schedule.getFactory().createTransitRoute(
+                                    Id.create(currentDirection, TransitRoute.class), nRoute, stops, "pt");
+                            tLine.addRoute(tRoute);
+                            startLink = null;
+                            links.clear();
+                            endLink = null;
+                            stops.clear();
+                        }
 
-						tStopFacil = createTransitStopFacility(iniNode, startLink);
-						// ad hoc solution, mentioned above
-						if (!schedule.getFacilities().containsKey(tStopFacil.getId()) && !line.equals("T40"))
-							schedule.addStopFacility(tStopFacil);
-						tRouteStop = schedule.getFactory().createTransitRouteStop(tStopFacil, 0, 0);
-						tRouteStop.setAwaitDepartureTime(true);
-						stops.add(tRouteStop);
+                        currentDirection = direction;
 
-						links.add(Id.createLinkId(iniNode.getId().toString() + "-" + iniStation));
-					}
-					if (!line.equals(currentLine)) {
-						if (currentLine != "")
-							schedule.addTransitLine(tLine);
+                        startLink = Id.createLinkId(iniStation + "-" + iniNode.getId().toString());
 
-						tLine = schedule.getFactory().createTransitLine(Id.create(line, TransitLine.class));
+                        tStopFacil = createTransitStopFacility(iniNode, startLink);
+                        // ad hoc solution, mentioned above
+                        if (!schedule.getFacilities().containsKey(tStopFacil.getId()) && !line.equals("T40"))
+                            schedule.addStopFacility(tStopFacil);
+                        tRouteStop = schedule.getFactory().createTransitRouteStop(tStopFacil, 0, 0);
+                        tRouteStop.setAwaitDepartureTime(true);
+                        stops.add(tRouteStop);
 
-						currentLine = line;
-					}
+                        links.add(Id.createLinkId(iniNode.getId().toString() + "-" + iniStation));
+                    }
+                    if (!line.equals(currentLine)) {
+                        if (currentLine != "") schedule.addTransitLine(tLine);
 
-					links.add(Id.createLinkId(iniStation + "-" + endStation));
-					links.add(Id.createLinkId(endStation + "-" + endNode.getId().toString()));
+                        tLine = schedule.getFactory().createTransitLine(Id.create(line, TransitLine.class));
 
-					tStopFacil = createTransitStopFacility(endNode, Id.createLinkId(endStation + "-" + endNode.getId().toString()));
-					// ad hoc solution, mentioned above
-					if (!schedule.getFacilities().containsKey(tStopFacil.getId()) && !line.equals("T40"))
-						schedule.addStopFacility(tStopFacil);
-					tRouteStop = schedule.getFactory().createTransitRouteStop(tStopFacil, 0, 0);
-					tRouteStop.setAwaitDepartureTime(true);
-					stops.add(tRouteStop);
+                        currentLine = line;
+                    }
 
-					links.add(Id.createLinkId(endNode.getId().toString() + "-" + endStation));
-					endLink = Id.createLinkId(endNode.getId().toString() + "-" + endStation);
-				}
+                    links.add(Id.createLinkId(iniStation + "-" + endStation));
+                    links.add(Id.createLinkId(endStation + "-" + endNode.getId().toString()));
 
-			}
-			arcos.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+                    tStopFacil = createTransitStopFacility(endNode,
+                            Id.createLinkId(endStation + "-" + endNode.getId().toString()));
+                    // ad hoc solution, mentioned above
+                    if (!schedule.getFacilities().containsKey(tStopFacil.getId()) && !line.equals("T40"))
+                        schedule.addStopFacility(tStopFacil);
+                    tRouteStop = schedule.getFactory().createTransitRouteStop(tStopFacil, 0, 0);
+                    tRouteStop.setAwaitDepartureTime(true);
+                    stops.add(tRouteStop);
 
-		// Create departures
-		try {
+                    links.add(Id.createLinkId(endNode.getId().toString() + "-" + endStation));
+                    endLink = Id.createLinkId(endNode.getId().toString() + "-" + endStation);
+                }
 
-			String lineas[] = { "T31", "E31", "E21" };
-			for (String linea : lineas) {
-				CsvReader salidas = new CsvReader("./input/networkMIO/" + linea + ".csv");
-				System.out.println("### extracting data from " + salidas.toString() + " ###");
-				salidas.readHeaders();
-				int id_dep = 1;
+            }
+            arcos.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
 
-				while (salidas.readRecord())
+        // Create departures
+        try {
 
-				{
-					Id<TransitLine> lineId = Id.create(linea, TransitLine.class);
+            String lineas[] = { "T31", "E31", "E21" };
+            for (String linea : lineas) {
+                CsvReader salidas = new CsvReader("./input/networkMIO/" + linea + ".csv");
+                System.out.println("### extracting data from " + salidas.toString() + " ###");
+                salidas.readHeaders();
+                int id_dep = 1;
 
-					Departure ida = schedule.getFactory().createDeparture(Id.create(id_dep, Departure.class), Double.parseDouble(salidas.get("Hora_mat")));
-					ida.setVehicleId(Id.create(salidas.get("Veh_ida"), org.matsim.vehicles.Vehicle.class));
-					schedule.getTransitLines().get(lineId).getRoutes().get(Id.create("Ida", TransitRoute.class)).addDeparture(ida);
+                while (salidas.readRecord()) {
+                    Id<TransitLine> lineId = Id.create(linea, TransitLine.class);
 
-					Departure regreso = schedule.getFactory().createDeparture(Id.create(id_dep, Departure.class), Double.parseDouble(salidas.get("Hora_mat")));
-					regreso.setVehicleId(Id.create(salidas.get("Veh_regreso"), org.matsim.vehicles.Vehicle.class));
-					schedule.getTransitLines().get(lineId).getRoutes().get(Id.create("Vuelta", TransitRoute.class)).addDeparture(regreso);
+                    Departure ida = schedule.getFactory().createDeparture(Id.create(id_dep, Departure.class),
+                            Double.parseDouble(salidas.get("Hora_mat")));
+                    ida.setVehicleId(Id.create(salidas.get("Veh_ida"), org.matsim.vehicles.Vehicle.class));
+                    schedule.getTransitLines().get(lineId).getRoutes().get(Id.create("Ida", TransitRoute.class))
+                            .addDeparture(ida);
 
-					id_dep++;
-				}
-				salidas.close();
-			}
+                    Departure regreso = schedule.getFactory().createDeparture(Id.create(id_dep, Departure.class),
+                            Double.parseDouble(salidas.get("Hora_mat")));
+                    regreso.setVehicleId(Id.create(salidas.get("Veh_regreso"), org.matsim.vehicles.Vehicle.class));
+                    schedule.getTransitLines().get(lineId).getRoutes().get(Id.create("Vuelta", TransitRoute.class))
+                            .addDeparture(regreso);
 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+                    id_dep++;
+                }
+                salidas.close();
+            }
 
-	public TransitStopFacility createTransitStopFacility(Node node, Id<Link> linkId)
-	{
-		TransitStopFacility tStopFacil = schedule.getFactory().createTransitStopFacility(Id.create(node.getId(), TransitStopFacility.class), node.getCoord(), true);
-		tStopFacil.setLinkId(linkId);
-		tStopFacil.setName((String) nodeAttributes.getAttribute(node.getId().toString(), "LONGNAME"));
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-		return tStopFacil;
-	}
+    public TransitStopFacility createTransitStopFacility(Node node, Id<Link> linkId) {
+        TransitStopFacility tStopFacil = schedule.getFactory()
+                .createTransitStopFacility(Id.create(node.getId(), TransitStopFacility.class), node.getCoord(), true);
+        tStopFacil.setLinkId(linkId);
+        tStopFacil.setName((String) nodeAttributes.getAttribute(node.getId().toString(), "LONGNAME"));
+
+        return tStopFacil;
+    }
 }
