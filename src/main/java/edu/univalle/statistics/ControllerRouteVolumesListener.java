c5403d0e93d229bf0968c9005f39c2ca48992727
Andres Gomez <fgomboe@gmail.com>
2016-06-22 13:21:36 -0500

    Change class 'ControllerRouteVolumesListener.java' to compute GEH

--- src/main/java/edu/univalle/statistics/ControllerRouteVolumesListener.java
+++ src/main/java/edu/univalle/statistics/ControllerRouteVolumesListener.java
@@ -2,8 +2,11 @@ package edu.univalle.statistics;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Id;
@@ -14,6 +17,7 @@ import org.matsim.core.controler.listener.IterationEndsListener;
 import org.matsim.core.controler.listener.StartupListener;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
+import edu.univalle.utils.CsvReader;
 import edu.univalle.utils.CsvWriter;
 
 public class ControllerRouteVolumesListener implements StartupListener, IterationEndsListener
@@ -26,12 +30,31 @@ public class ControllerRouteVolumesListener implements StartupListener, Iteratio
     private final int endTime;
     private RouteVolumesPax analyzer;
     private CsvWriter writer;
+    private CsvReader reader;
     Controler controler;
     private static final String volumeVariables[] = { "id", "entering", "leaving", "passthrough", "totalVolume" };
+    private Map<String, List<FacilityGEH>> lineRouteFacilityGEH = new HashMap<>();
 
     public ControllerRouteVolumesListener(int startTime, int endTime) {
         this.startTime = startTime;
         this.endTime = endTime;
+    }
+
+    private class FacilityGEH
+    {
+        Id<TransitStopFacility> facilityId;
+        double entering;
+        double leaving;
+        double passthrough;
+        double totalVolume;
+
+        FacilityGEH(Id<TransitStopFacility> id) {
+            this.facilityId = id;
+            this.entering = 0.0;
+            this.leaving = 0.0;
+            this.passthrough = 0.0;
+            this.totalVolume = 0.0;
+        }
     }
 
     @Override
@@ -49,12 +72,53 @@ public class ControllerRouteVolumesListener implements StartupListener, Iteratio
             // write results to external csv file
             openWriter("stats/Volumes-" + lineRoute + "_" + (int) Math.ceil((double) startTime / 3600) + "-"
                     + (int) Math.ceil((double) endTime / 3600) + ".csv");
-            writeVolumes(lineRoute);
+            writeSimVolumes(lineRoute);
             closeWriter();
             log.info("Results written to file: stats/Volumes-" + lineRoute + "_"
                     + (int) Math.ceil((double) startTime / 3600) + "-" + (int) Math.ceil((double) endTime / 3600)
                     + ".csv");
+
+            // compute GEH index for every measurement
+            openReader("stats/input/Volumes-" + lineRoute + "_" + (int) Math.ceil((double) startTime / 3600) + "-"
+                    + (int) Math.ceil((double) endTime / 3600) + ".csv");
+            List<Id<TransitStopFacility>> facilities = analyzer.getFacilities(lineRoute);
+            for (Id<TransitStopFacility> facility : facilities) {
+                int[] simVolumes = analyzer.getVolumesForRouteAndFacility(lineRoute, facility);
+                int[] realVolumes = readRealVolumes();
+                if (!lineRouteFacilityGEH.containsKey(lineRoute)) {
+                    List<FacilityGEH> facilityGEH = new ArrayList<>();
+                    FacilityGEH facGEH = new FacilityGEH(facility);
+                    facGEH.entering = calcGEH(simVolumes[0], realVolumes[0]);
+                    facGEH.leaving = calcGEH(simVolumes[1], realVolumes[1]);
+                    facGEH.passthrough = calcGEH(simVolumes[2], realVolumes[2]);
+                    facGEH.totalVolume = calcGEH(simVolumes[3], realVolumes[3]);
+                    facilityGEH.add(facGEH);
+                    lineRouteFacilityGEH.put(lineRoute, facilityGEH);
+                }
+                else if (checkIdInFacilityVolumeList(facility, lineRouteFacilityGEH.get(lineRoute)) == -1) {
+                    FacilityGEH facGEH = new FacilityGEH(facility);
+                    facGEH.entering = calcGEH(simVolumes[0], realVolumes[0]);
+                    facGEH.leaving = calcGEH(simVolumes[1], realVolumes[1]);
+                    facGEH.passthrough = calcGEH(simVolumes[2], realVolumes[2]);
+                    facGEH.totalVolume = calcGEH(simVolumes[3], realVolumes[3]);
+                    lineRouteFacilityGEH.get(lineRoute).add(facGEH);
+                }
+                System.out.println("listo con los GEHs");
+            }
         }
+    }
+
+    private int checkIdInFacilityVolumeList(Id<TransitStopFacility> id, List<FacilityGEH> list) {
+        if (id == null)
+            return -1;
+
+        int pos = 0;
+        for (FacilityGEH facGEH : list) {
+            if (facGEH.facilityId.toString().equals(id.toString()))
+                return pos;
+            pos++;
+        }
+        return -1;
     }
 
     private void openWriter(String outputFile) {
@@ -74,7 +138,7 @@ public class ControllerRouteVolumesListener implements StartupListener, Iteratio
         writer.close();
     }
 
-    private void writeVolumes(String lineRoute) {
+    private void writeSimVolumes(String lineRoute) {
         List<Id<TransitStopFacility>> facilities = analyzer.getFacilities(lineRoute);
         for (Id<TransitStopFacility> facility : facilities) {
             try {
@@ -87,5 +151,40 @@ public class ControllerRouteVolumesListener implements StartupListener, Iteratio
                 e.printStackTrace();
             }
         }
+    }
+
+    private void openReader(String inputFile) {
+        try {
+            reader = new CsvReader(inputFile);
+            reader.readHeaders();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int[] readRealVolumes() {
+        int[] volumes = new int[4];
+        try {
+            reader.readRecord();
+            volumes[0] = reader.getIndex(volumeVariables[1]);
+            volumes[1] = reader.getIndex(volumeVariables[2]);
+            volumes[2] = reader.getIndex(volumeVariables[3]);
+            volumes[3] = reader.getIndex(volumeVariables[4]);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return volumes;
+    }
+
+    private double calcGEH(double sim, double real) {
+        double sqDiff = Math.pow(sim - real, 2.0);
+        double sum = sim + real;
+        return Math.sqrt(2 * (sqDiff / sum));
     }
 }
