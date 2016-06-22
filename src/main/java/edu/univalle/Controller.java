424d3687ed94a99bbf3e2fec7fd6d21792de7457
Andres Gomez <fgomboe@gmail.com>
2016-06-22 12:56:17 -0500

    Leave only basic controller

--- src/main/java/edu/univalle/Controller.java
+++ src/main/java/edu/univalle/Controller.java
@@ -4,15 +4,12 @@ import org.matsim.core.config.Config;
 import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 
-import edu.univalle.statistics.ControllerLinkVolumesListener;
-
 public class Controller
 {
 
     public static void main(String[] args) {
         Config config = ConfigUtils.loadConfig("input/config_dummy.xml");
         Controler controler = new Controler(config);
-        controler.addControlerListener(new ControllerLinkVolumesListener(61200, 68399, 3600));
         controler.run();
     }
 
